8cdaf9d53158d1ecd91ce029e67f2854295c5d36
ThurstonLim <43884985+ThurstonLim@users.noreply.github.com>
2020-03-02 17:18:35 -0500

    Update cps5790test.py

--- cps5790test.py
+++ cps5790test.py
@@ -1,5 +1,11 @@
 import unittest
 
-def test(self):
-        self.assertEqual(cps5790(7), 42)
-  
+import cps5790.py
+
+class Test(unittest.TestCase):
+        def test(self):
+                self.assertEqual(cps5790(7), 42)
+
+
+if __name__ == '__main__':
+    unittest.main()
