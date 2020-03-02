6120ed5d9ebfdcb8ffc563b02f3d6a811c66d2f9
ThurstonLim <43884985+ThurstonLim@users.noreply.github.com>
2020-03-02 17:30:37 -0500

    Update cps5790test.py

--- cps5790test.py
+++ cps5790test.py
@@ -3,7 +3,7 @@ import unittest
 import cps5790
 
 class Test(unittest.TestCase):
-        def test_increment_one_1(self):
+        def test(self):
                 self.assertEqual( cps5790(7), 42)
 
 
